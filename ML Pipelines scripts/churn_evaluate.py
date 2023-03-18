e4b2a7b4ec7db9dec16c6621ae51b80b8f6a91eb
Abhijit <abhijit.kalita@gmail.com>
2023-03-18 12:28:05 +0000

    Added a new folder for pipeline

--- ML Pipelines scripts/churn_evaluate.py
+++ ML Pipelines scripts/churn_evaluate.py
@@ -8,28 +8,71 @@ import numpy as np
 import pandas as pd
 import xgboost
 import datetime as dt
-from sklearn.metrics import roc_curve,auc
+import logging
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    confusion_matrix,
+    roc_curve,
+    auc
+)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
 if __name__ == "__main__":   
+    
     #Read Model Tar File
+    logger.debug("Loading xgboost model.")
     model_path = f"/opt/ml/processing/model/model.tar.gz"
     with tarfile.open(model_path) as tar:
         tar.extractall(path=".")
     model = pickle.load(open("xgboost-model", "rb"))
+    
     #Read Test Data using which we evaluate the model
+    logger.debug("Loading test input data.")
     test_path = "/opt/ml/processing/test/test.csv"
     df = pd.read_csv(test_path, header=None)
     y_test = df.iloc[:, 0].to_numpy()
     df.drop(df.columns[0], axis=1, inplace=True)
     X_test = xgboost.DMatrix(df.values)
+    
     #Run Predictions
     predictions = model.predict(X_test)
+    
     #Evaluate Predictions
-    fpr, tpr, thresholds = roc_curve(y_test, predictions)
+    logger.info("Performing predictions against test data.")
+    prediction_probabilities = model.predict(X_test)
+    predictions = np.round(prediction_probabilities)
+
+    precision = precision_score(y_test, predictions, zero_division=1)
+    recall = recall_score(y_test, predictions)
+    accuracy = accuracy_score(y_test, predictions)
+    conf_matrix = confusion_matrix(y_test, predictions)
+    fpr, tpr, _ = roc_curve(y_test, prediction_probabilities)
     auc_score = auc(fpr, tpr)
+
+
+    logger.debug("Accuracy: {}".format(accuracy))
+    logger.debug("Precision: {}".format(precision))
+    logger.debug("Recall: {}".format(recall))
+    logger.debug("Confusion matrix: {}".format(conf_matrix))
+    
     report_dict = {
-        "classification_metrics": {
-            "auc_score": {
-                "value": auc_score,
+        "binary_classification_metrics": {
+            "accuracy": {"value": accuracy, "standard_deviation": "NaN"},
+            "precision": {"value": precision, "standard_deviation": "NaN"},
+            "recall": {"value": recall, "standard_deviation": "NaN"},
+            "auc_score": {"value": auc_score},
+            "confusion_matrix": {
+                "0": {"0": int(conf_matrix[0][0]), "1": int(conf_matrix[0][1])},
+                "1": {"0": int(conf_matrix[1][0]), "1": int(conf_matrix[1][1])},
+            },
+            "receiver_operating_characteristic_curve": {
+                "false_positive_rates": list(fpr),
+                "true_positive_rates": list(tpr),
             },
         },
     }
